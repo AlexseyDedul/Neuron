6bb8369a352c7c0c7382d2ae81e0f150924f398b
AlexseyDedul <dedyul@gmail.com>
2020-07-30 08:14:51 +0300

    Scalling&Normalize
    разработаны методы масштабирования и нормализации нейронной сети

--- NeuronNetworks/NeuronNetworks/Program.cs
+++ NeuronNetworks/NeuronNetworks/Program.cs
@@ -10,7 +10,8 @@ namespace NeuronNetworks
     {
         static void Main()
         {
-            var dataset = new List<Tuple<double, double[]>>
+            var outputs = new double[] { 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1 };
+            var inputs = new double[,]
             {
                 // Результат пациента - пациент болен - 1
                 //                      пациент здоров - 0
@@ -18,24 +19,23 @@ namespace NeuronNetworks
                 // Хороший возраст А
                 // курит S 
                 // правильно питается F
-                //                                             T  A  S  F
-                new Tuple<double, double[]> (0, new double[] { 0, 0, 0, 0 }),
-                new Tuple<double, double[]> (0, new double[] { 0, 0, 0, 1 }),
-                new Tuple<double, double[]> (1, new double[] { 0, 0, 1, 0 }),
-                new Tuple<double, double[]> (0, new double[] { 0, 0, 1, 1 }),
-                new Tuple<double, double[]> (0, new double[] { 0, 1, 0, 0 }),
-                new Tuple<double, double[]> (0, new double[] { 0, 1, 0, 1 }),
-                new Tuple<double, double[]> (1, new double[] { 0, 1, 1, 0 }),
-                new Tuple<double, double[]> (0, new double[] { 0, 1, 1, 1 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 0, 0, 0 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 0, 0, 1 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 0, 1, 0 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 0, 1, 1 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 1, 0, 0 }),
-                new Tuple<double, double[]> (0, new double[] { 1, 1, 0, 1 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 1, 1, 0 }),
-                new Tuple<double, double[]> (1, new double[] { 1, 1, 1, 1 })
-
+                //T  A  S  F
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 1 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 1, 1 },
+                { 0, 1, 0, 0 },
+                { 0, 1, 0, 1 },
+                { 0, 1, 1, 0 },
+                { 0, 1, 1, 1 },
+                { 1, 0, 0, 0 },
+                { 1, 0, 0, 1 },
+                { 1, 0, 1, 0 },
+                { 1, 0, 1, 1 },
+                { 1, 1, 0, 0 },
+                { 1, 1, 0, 1 },
+                { 1, 1, 1, 0 },
+                { 1, 1, 1, 1 }
             };
 
             var topology = new Topology(4, 1, 0.1, 2);
@@ -43,18 +43,20 @@ namespace NeuronNetworks
             //neuralNetwork.Layers[1].Neurons[0].SetWeights(0.5, -0.1, 0.3, -0.1);
             //neuralNetwork.Layers[1].Neurons[1].SetWeights(0.1, -0.3, 0.7, -0.3);
             //neuralNetwork.Layers[2].Neurons[0].SetWeights(1.2, 0.8);
-            var differense = neuralNetwork.Learn(dataset, 10000);
+            var differense = neuralNetwork.Learn(outputs, inputs, 10000);
 
             var results = new List<double>();
-            foreach(var data in dataset)
+
+            for(int i = 0; i < outputs.Length; i++)
             {
-                results.Add(neuralNetwork.FeedForward(data.Item2).Output);
+                var row = NeuralNetwork.GetRow(inputs, i);
+                results.Add(neuralNetwork.FeedForward(row).Output);
             }
 
             for(int  i = 0; i < results.Count; i++)
             {
-                var expected = Math.Round(dataset[i].Item1, 3);
-                var actual = Math.Round(results[i], 3);
+                var expected = Math.Round(outputs[i], 2);
+                var actual = Math.Round(results[i], 2);
                 Console.WriteLine(expected + "   " + actual);
                 
             }
